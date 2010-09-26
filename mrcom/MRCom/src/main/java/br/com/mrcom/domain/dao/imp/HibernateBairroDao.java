5ecf80ce5db802491787f71bcbfae59bd816f307
dariosena <dariosena@0e11b3c3-a9d6-0a2a-a29c-e8394e4c8fb8>
2010-09-26 18:37:16 +0000

    Adicionando novas interfaces dos daos vale lembrar q existe varios daos e interfaces pq pode haver necessidade de um novo metodo em um dao especifico.

---
+++ mrcom/MRCom/src/main/java/br/com/mrcom/domain/dao/imp/HibernateBairroDao.java
@@ -0,0 +1,8 @@
+package br.com.mrcom.domain.dao.imp;
+
+import br.com.mrcom.domain.bean.Bairro;
+import br.com.mrcom.domain.dao.BairroDao;
+
+public class HibernateBairroDao extends HibernateGenericDao<Bairro> implements BairroDao {
+
+}
