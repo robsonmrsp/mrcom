4c96e5624fe3c9ff28f1bda591a5ddf9f36dfe44
robsonmrsp <robsonmrsp@0e11b3c3-a9d6-0a2a-a29c-e8394e4c8fb8>
2010-09-26 01:12:49 +0000

    Adicao de novas classes e do ANT ao projeto

--- trunk/mrcom/MRCom/src/test/java/br/com/comerce/HelloWorldTest.java
+++ trunk/mrcom/MRCom/src/test/java/br/com/comerce/HelloWorldTest.java
@@ -1,12 +1,13 @@
 package br.com.comerce;
 
-import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
+
 
 import br.com.mrcom.HelloWorld;
 
 public class HelloWorldTest
 {
-   @Test
+//   @Test
    public void testGetText() {
       HelloWorld fixture = new HelloWorld();
       assert "Hello World!".equals(fixture.getText());
